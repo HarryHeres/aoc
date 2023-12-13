cd3086512833d6ec239b5d1a11ed383e8d905ae9
HarryHeres <personal@janheres.eu>
2023-12-13 14:52:50 +0100

    Working on Day 13 part 2

--- 2023/day_13/Sources/main.swift
+++ 2023/day_13/Sources/main.swift
@@ -17,8 +17,8 @@ func transpose_map(map: [String]) -> [String] {
     return transposed
 }
 
-func find_reflection(map: [String]) -> UInt32 {
-    var rv: UInt32 = 0 
+func find_reflection(previous_reflection: Int, map: [String]) -> UInt32 {
+    var rv: UInt32 = 0
     let lineLen = map[0].count
   
     outer: for j in 1..<lineLen {
@@ -30,30 +30,40 @@ func find_reflection(map: [String]) -> UInt32 {
                 let leftIdx = currLine.index(separatorIdx, offsetBy: -k)
                 let rightIdx = currLine.index(separatorIdx, offsetBy: k - 1)
                 if (currLine[leftIdx] != currLine[rightIdx]){
-                    print("Difference at line \(i + 1): [\(currLine[leftIdx]);\(j - k)] != [\(currLine[rightIdx]);\(j + k - 1)]")
+                    // print("Difference at line \(i + 1): [\(currLine[leftIdx]);\(j - k)] != [\(currLine[rightIdx]);\(j + k - 1)]")
                     continue outer
                 }
             } 
         }
-
+        
         // print("Found reflection between \(j) and \(j + 1)")
-        rv = UInt32(j)
-        break
+        if previous_reflection == -1 {
+            rv = UInt32(j)
+            break
+        }
+        else {
+            if j != previous_reflection { // Found a different solution, no need to continue
+                rv = UInt32(j)
+                break
+            }
+            else {
+                continue // Found previous solution, continue to find another one
+            }
+        }
     }
 
     return rv
 }
 
-
-func part_one(maps: [[String]]) -> UInt32 {
+func part_two(prev: inout [UInt32], maps: [[String]]) -> UInt32 {
     var sum: UInt32 = 0 
     
     for i in 0..<maps.count {
         let map = maps[i]
-        var curr = find_reflection(map: map) 
+        var curr = find_reflection(previous_reflection: Int(prev[i]), map: map) 
         if curr == 0 { // No reflection found between columns, transpose and try again for rows
             let transposed = transpose_map(map: map)
-            curr += find_reflection(map: transposed) * 100 
+            curr = find_reflection(previous_reflection: Int(prev[i]) , map: transposed) * 100 
         }
 
         if curr == 0 {
@@ -65,6 +75,30 @@ func part_one(maps: [[String]]) -> UInt32 {
     }
 
     return sum
+}
+
+
+
+func part_one(maps: [[String]]) -> [UInt32] {
+    var differences: [UInt32] = [UInt32]() 
+    
+    for i in 0..<maps.count {
+        let map = maps[i]
+        var curr = find_reflection(previous_reflection: -1, map: map) 
+        if curr == 0 { // No reflection found between columns, transpose and try again for rows
+            let transposed = transpose_map(map: map)
+            curr = find_reflection(previous_reflection: -1, map: transposed) * 100 
+        }
+
+        if curr == 0 {
+            print("No reflection found for map \(i+1)")
+        }
+        
+        print("Score for map \(i + 1): \(curr)")
+        differences.append(curr)
+    }
+
+    return differences 
 }
 
 func parse_maps(contents: [String.SubSequence]) -> [[String]] {
@@ -93,8 +127,12 @@ func main(){
     let maps: [[String]] = parse_maps(contents: contents)
     print("Parsed \(maps.count) maps")
 
-    let sum: UInt32 = part_one(maps: maps)
+    var prev: [UInt32] = part_one(maps: maps)
+    var sum: UInt32 = prev.reduce(0, +)
     print("Sum (part one): \(sum)")
+
+    sum = part_two(prev: &prev, maps: maps)
+    print("Sum (part two): \(sum)")
 }
 
 main()
