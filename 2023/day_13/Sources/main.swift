55640a2951e947cf24f58497635f51dc904c97c4
HarryHeres <personal@janheres.eu>
2023-12-13 14:14:11 +0100

    Day 13 Part 1 done

--- 2023/day_13/Sources/main.swift
+++ 2023/day_13/Sources/main.swift
@@ -7,7 +7,7 @@ func transpose_map(map: [String]) -> [String] {
         var nextRow: String = String()
         for rowIdx in 0..<map.count {
             let currLine = map[rowIdx]
-            let j: String.Index = currLine.index(currLine.startIndex, offsetBy: colIdx)
+            let j: String.Index = currLine.index(currLine.startIndex, offsetBy: currLine.count - 1 - colIdx)
             let char: Character = map[rowIdx][j]
             nextRow.append(char)
         }
@@ -21,23 +21,24 @@ func find_reflection(map: [String]) -> UInt32 {
     var rv: UInt32 = 0 
     let lineLen = map[0].count
   
-    outer: for j in (lineLen / 2)..<lineLen - 1 {
+    outer: for j in 1..<lineLen {
+        // print("Separator between \(j) and \(j + 1)")
         for i in 0..<map.count { 
             let currLine = map[i]
-            let separatorIdx = currLine.index(currLine.startIndex, offsetBy:j)
-            for k in 1...lineLen - j{ // Just need to check only from half the line
-                // Check if the right half is reflected properly
+            let separatorIdx = currLine.index(currLine.startIndex, offsetBy: j)
+            for k in 1...min(j, lineLen - j){ 
                 let leftIdx = currLine.index(separatorIdx, offsetBy: -k)
-                let rightIdx = currLine.index(separatorIdx, offsetBy: k)
+                let rightIdx = currLine.index(separatorIdx, offsetBy: k - 1)
                 if (currLine[leftIdx] != currLine[rightIdx]){
+                    print("Difference at line \(i + 1): [\(currLine[leftIdx]);\(j - k)] != [\(currLine[rightIdx]);\(j + k - 1)]")
                     continue outer
                 }
             } 
         }
 
-        // Found a reflection
-        rv = UInt32(lineLen - j);        
-        print("Found reflection between columns \(j) and \(j + 1)")
+        // print("Found reflection between \(j) and \(j + 1)")
+        rv = UInt32(j)
+        break
     }
 
     return rv
@@ -47,13 +48,19 @@ func find_reflection(map: [String]) -> UInt32 {
 func part_one(maps: [[String]]) -> UInt32 {
     var sum: UInt32 = 0 
     
-    for map in maps {
-        var curr = find_reflection(map: map)     
+    for i in 0..<maps.count {
+        let map = maps[i]
+        var curr = find_reflection(map: map) 
         if curr == 0 { // No reflection found between columns, transpose and try again for rows
             let transposed = transpose_map(map: map)
-            curr = find_reflection(map: transposed) * 100
+            curr += find_reflection(map: transposed) * 100 
         }
 
+        if curr == 0 {
+            print("No reflection found for map \(i+1)")
+        }
+        
+        print("Score for map \(i + 1): \(curr)")
         sum += curr
     }
 
@@ -69,7 +76,10 @@ func parse_maps(contents: [String.SubSequence]) -> [[String]] {
             parsed.append(curr)
             curr = [String]()
         }
-        curr.append(String(line))
+        else{
+            curr.append(line.replacingOccurrences(of: ";", with: ""))
+        }
+
     })
 
     return parsed
@@ -77,9 +87,11 @@ func parse_maps(contents: [String.SubSequence]) -> [[String]] {
 
 
 func main(){
-    let file: String = "input_example.txt"
-    let contents: [String.SubSequence] = try! String(contentsOfFile: file).split(separator: "\n") 
+    let file: String = "input.txt"
+    let contents: [String.SubSequence] = try! String(contentsOfFile: file).replacingOccurrences(of: "\n", with: ";\n").split(separator: "\n")
+
     let maps: [[String]] = parse_maps(contents: contents)
+    print("Parsed \(maps.count) maps")
 
     let sum: UInt32 = part_one(maps: maps)
     print("Sum (part one): \(sum)")
