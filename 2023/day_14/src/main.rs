82cffd30659eb59999736cc206d5fb28dfe14550
HarryHeres <personal@janheres.eu>
2023-12-15 20:59:23 +0100

    Day 14 part 1 done

--- 2023/day_14/src/main.rs
+++ 2023/day_14/src/main.rs
@@ -11,22 +11,23 @@ fn part_one(reader: &mut BufReader<File>) -> u32 {
         .read_to_string(&mut lines)
         .expect("Could not read the input file contents into a string");
 
-    let line_count = lines.split("\n").count();
+    let line_count = lines.split("\n").count() - 1;
     let line_len = lines.find("\n").unwrap() + 1;
 
     let mut idx = 0;
     let mut map: HashMap<usize, usize> = HashMap::new(); // Old position, new position
 
-    let mut lines = lines.chars();
+    let lines = lines.chars();
     lines.clone().for_each(|char| {
         if char == 'O' {
             let mut lookup_idx: usize = idx;
-            if lookup_idx < line_len {
+            if idx < line_len {
                 map.insert(idx, line_count);
             } else {
-                while lookup_idx > line_len {
+                loop {
                     lookup_idx -= line_len;
-                    match lines.nth(lookup_idx) {
+
+                    match lines.clone().nth(lookup_idx) {
                         Some(val) => match val {
                             'O' => {
                                 map.insert(idx, map[&lookup_idx] - 1);
@@ -36,7 +37,13 @@ fn part_one(reader: &mut BufReader<File>) -> u32 {
                                 map.insert(idx, line_count - lookup_idx / line_len - 1);
                                 break;
                             }
-                            _ => continue,
+                            _ => {
+                                if lookup_idx < line_len && map.get(&lookup_idx) == None {
+                                    map.insert(idx, line_count);
+                                    break;
+                                }
+                                continue;
+                            }
                         },
                         None => {
                             map.insert(idx, line_count);
@@ -53,7 +60,7 @@ fn part_one(reader: &mut BufReader<File>) -> u32 {
 }
 
 fn main() {
-    let file: File = File::open("input_example.txt").expect("Could not open the input file");
+    let file: File = File::open("input.txt").expect("Could not open the input file");
     let mut reader: BufReader<File> = BufReader::new(file);
 
     let sum: u32 = part_one(&mut reader);
