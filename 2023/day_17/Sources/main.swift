740547afabce76c4d2d95892830a85b98e0d8954
HarryHeres <personal@janheres.eu>
2023-12-17 14:05:25 +0100

    Beginning Day 17

---
+++ 2023/day_17/Sources/main.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+func partOne(input: String) -> UInt32 {
+    var lowest: UInt32 = 0
+   return lowest 
+}
+
+func main() {
+    let filename: String = "input_example.txt"
+    let contents: String = try! String(contentsOfFile: filename)
+
+}
+
+main()
