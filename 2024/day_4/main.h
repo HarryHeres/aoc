009da98f3ec8fef5f86ad113dcc1262d951cc9a7
HarryHeres <personal@janheres.eu>
2024-12-05 13:28:26 +0100

    Day 4 Part 1 done

--- 2024/day_4/main.h
+++ 2024/day_4/main.h
@@ -11,5 +11,14 @@ const uint find_reverse(const char* buffer, const uint start_idx);
 
 const uint find_above(const char* buffer, const uint start_idx, const uint line_len);
 
+const uint find_above_left(const char* buffer, const uint start_idx, const uint line_len);
+
+const uint find_above_right(const char* buffer, const uint start_idx, const uint line_len);
+
 const uint find_below(const char* buffer, const uint start_idx, const uint line_len);
+
+const uint find_below_left(const char* buffer, const uint start_idx, const uint line_len);
+
+const uint find_below_right(const char* buffer, const uint start_idx, const uint line_len);
+
 #endif
