96815e6724c89bf562bbf23b4b919e156472ce75
Jan Hereš <personal@janheres.eu>
2024-12-06 23:32:17 +0100

    [WIP] Day 6 Part 2

--- 2024/day_6/include/main.h
+++ 2024/day_6/include/main.h
@@ -1,9 +1,16 @@
 #ifndef _MAIN_
 #define _MAIN_
 
+#include <stdbool.h>
 #include <stdint.h>
 
 uint32_t traverse_map(char* map, const uint32_t rows, const uint32_t columns, const uint32_t start_x,
                       const uint32_t start_y);
+
+uint32_t find_obstruction_places(char* map, const uint32_t rows, const uint32_t columns, const uint32_t start_x,
+                                 const uint32_t start_y);
+
+bool contains_cycle(char* map, const uint32_t rows, const uint32_t columns, const uint32_t start_x,
+                    const uint32_t start_y);
 
 #endif
--- 2024/day_6/src/main.c
+++ 2024/day_6/src/main.c
@@ -8,12 +8,12 @@
 #include <sys/types.h>
 
 int main(void) {
-  FILE* file = fopen("data/input.txt", "r");
-  const uint16_t rows = 130;
-  const uint16_t columns = 130;
-  const uint32_t file_size = rows * columns;
+  FILE* file = fopen("data/input_test.txt", "r");
+  const uint16_t rows = 10;
+  const uint16_t columns = 10;
+  const uint32_t map_size = rows * columns;
 
-  char map[file_size];
+  char map[map_size];
   char line_buffer[columns + 2];  // newline + terminating character
 
   memset(map, '\0', sizeof(map));
@@ -44,11 +44,88 @@ int main(void) {
     return EXIT_FAILURE;
   }
 
-  const uint32_t result = traverse_map(map, rows, columns, start_x, start_y);
+  // Part one
+  /* const uint32_t result = traverse_map(map, rows, columns, start_x, start_y); */
+  /* printf("Result: %d\n", result); */
+
+  // Part two
+  const uint32_t result = find_obstruction_places(map, rows, columns, start_x, start_y);
   printf("Result: %d\n", result);
 
   fclose(file);
   return EXIT_SUCCESS;
+}
+
+uint32_t find_obstruction_places(char* map, const uint32_t rows, const uint32_t columns, const uint32_t start_x,
+                                 const uint32_t start_y) {
+  uint32_t result = 0;
+  for (uint32_t i = 0; i < rows * columns; ++i) {
+    if (map[i] == '.') {
+      printf("Testing %d\n", i);
+      map[i] = '#';
+
+      if (contains_cycle(map, rows, columns, start_x, start_y) == true) {
+        ++result;
+        printf("Cycle found on %d\n", i);
+      }
+
+      map[i] = '.';
+    }
+  }
+
+  return result;
+}
+
+bool contains_cycle(char* map, const uint32_t rows, const uint32_t columns, const uint32_t start_x,
+                    const uint32_t start_y) {
+  bool rv = false;
+  uint16_t direction = 0;
+
+  uint32_t curr_x = start_x;
+  uint32_t curr_y = start_y;
+
+  char* curr_char = NULL;
+  char next_char;
+  char map_copy[rows * columns];
+  memcpy(map_copy, map, rows * columns);
+
+  while (true) {
+    if (curr_x == 0 || curr_x == rows || curr_y == 0 || curr_y == columns) {
+      break;
+    }
+
+    curr_char = &map_copy[curr_x + (curr_y * columns)];
+    next_char = map_copy[(curr_x + DIRECTIONS[direction].x) + ((curr_y + DIRECTIONS[direction].y) * columns)];
+    if (next_char == '#') {
+      direction = (direction + 1) % (sizeof(DIRECTIONS) / sizeof(Direction));
+      next_char = map_copy[(curr_x + DIRECTIONS[direction].x) + ((curr_y + DIRECTIONS[direction].y) * columns)];
+    }
+
+    if (*curr_char == 'X' && next_char == 'X') {
+      rv = true;
+      break;
+    }
+
+    if (*curr_char != 'X') {
+      *curr_char = 'X';
+    }
+
+    curr_x += DIRECTIONS[direction].x;
+    curr_y += DIRECTIONS[direction].y;
+  }
+
+  if (rv == true) {
+    for (uint32_t i = 0; i < rows * columns; ++i) {
+      if (i > 0 && i % columns == 0) {
+        printf("\n");
+      }
+      printf("%c", map_copy[i]);
+    }
+    printf("\n");
+    printf("\n");
+  }
+
+  return rv;
 }
 
 uint32_t traverse_map(char* map, const uint32_t rows, const uint32_t columns, const uint32_t start_x,
