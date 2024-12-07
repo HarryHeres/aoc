d01872ef925072d7a293f0618721f2a7e6a77ad9
Jan Hereš <personal@janheres.eu>
2024-12-08 00:12:52 +0100

    Updating template

--- 2024/template/src/main.c
+++ 2024/template/src/main.c
@@ -8,8 +8,4 @@ int main(void) {
   FILE* file = fopen("data/input_test.txt", "r");
   const uint16_t file_size = 100;
   const uint16_t line_len = 10;  // including newline
-  char buffer[file_size] = {'\0'};
-
-  fread(buffer, sizeof(char), file_size, file);
-  printf("%s", buffer);
 }
