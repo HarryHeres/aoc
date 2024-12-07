778efd6b9729df678a2fa54664cec9fbd5aad270
Jan Hereš <personal@janheres.eu>
2024-12-08 00:13:20 +0100

    Preparing Day 8

---
+++ 2024/day_8/src/main.c
@@ -0,0 +1,11 @@
+#include <main.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/types.h>
+
+int main(void) {
+  FILE* file = fopen("data/input_test.txt", "r");
+  const uint16_t file_size = 100;
+  const uint16_t line_len = 10;  // including newline
+}
