9634824535aa8e327e12ab139b75dd141c8f4bc9
HarryHeres <personal@janheres.eu>
2024-12-10 23:12:40 +0100

    Updated template

--- 2024/template/include/main.h
+++ 2024/template/include/main.h
@@ -1,4 +1,11 @@
 #ifndef _MAIN_
 #define _MAIN_
 
+#include <stdint.h>
+#include <stdio.h>
+
+uint64_t part_one(FILE* file);
+
+uint64_t part_two(FILE* file);
+
 #endif
--- 2024/template/src/main.c
+++ 2024/template/src/main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 
 const uint32_t ROWS = 0;
@@ -10,5 +11,16 @@ const uint32_t COLS = 0;
 int main(void) {
   FILE* file = fopen("data/input_test.txt", "r");
 
-  printf("Hello, world!\n");
+  printf("Part one: %lld\n", part_one(file));
+  printf("Part two: %lld\n", part_two(file));
+
+  return EXIT_SUCCESS;
+}
+
+uint64_t part_one(FILE* file) {
+  return 0;
+}
+
+uint64_t part_two(FILE* file) {
+  return 0;
 }
