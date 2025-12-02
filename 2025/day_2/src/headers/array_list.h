858f597a126aae84308e34b3e03d21d79ffc7216
HarryHeres <personal@janheres.eu>
2025-12-02 21:18:54 +0100

    Day 2 complete

---
+++ 2025/day_2/src/headers/array_list.h
@@ -0,0 +1,24 @@
+#ifndef _ARRAY_LIST
+#define _ARRAY_LIST
+
+#include <stdio.h>
+
+typedef struct {
+  void *value;
+} ArrayListItem;
+
+typedef struct {
+  int size;
+  int capacity;
+  ArrayListItem *_items;
+} ArrayList;
+
+ArrayList *array_list_create(int initial_capacity);
+
+void array_list_add(ArrayList *list, void *item);
+
+void *array_list_get(const ArrayList *list, int index);
+
+void array_list_free(ArrayList *list);
+
+#endif
